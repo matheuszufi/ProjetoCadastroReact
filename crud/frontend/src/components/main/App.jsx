e8479e2c6a055d3477397097dfef8608ae52977a
Matheus Zufi <matheuszufi@gmail.com>
2022-05-11 14:36:18 -0300

    Update Routes Include

--- crud/frontend/src/components/main/App.jsx
+++ crud/frontend/src/components/main/App.jsx
@@ -1,16 +1,21 @@
 import './App.css'
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { HashRouter } from 'react-router-dom'
+
+import Routes from './Routes'
 
 import Logo from '../template/Logo'
 import Nav from '../template/Nav'
-import Home from '../home/Home'
+
 import Footer from '../template/Footer'
 
 export default props =>
+<HashRouter>
 <div className="app">
     <Logo />
     <Nav />
-    <Home />
+    <Routes />
     <Footer />
 </div>
+</HashRouter>
